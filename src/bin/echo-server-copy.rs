5406d8a69314c9fdeeb259faa33c45ee93121bd9
Ethan Backhus <backhusethan@gmail.com>
2025-12-11 14:30:03 -0500

    WIP

---
+++ src/bin/echo-server-copy.rs
@@ -0,0 +1,19 @@
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let (mut rd, mut wr) = socket.split();
+
+            if tokio::io::copy(&mut rd, &mut wr).await.is_err() {
+                eprint!("failed to copy");
+            };
+        });
+    }
+}
